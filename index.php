029a0fcf9107c7683b30b6be5e1507508618e0ff
Terique915 <54835492+Terique915@users.noreply.github.com>
2020-04-11 18:38:57 +0200

    php

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-echo "Hello World! :)";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HOMEPAGE</title>
+</head>
+<body>
+<h1>PHP voor beginners</h1>
+<h2> Terique Blijden </h2>
+</body>
+</html>
