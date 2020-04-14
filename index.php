e31666f1f8f1957ea883852428c6d3cc46a84052
Terique915 <54835492+Terique915@users.noreply.github.com>
2020-04-14 19:31:20 +0200

    php

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>HOMEPAGE</title>
+    <title>Homepage</title>
+
 </head>
 <body>
-<h1>PHP voor beginners</h1>
-<h2> Terique Blijden </h2>
+<h1>PHP Cursus</h1>
+<h2>Terique Blijden</h2>
+<div>
+    <ul>
+        <li><a href="H01/helloworld.php">Hoofdstuk 1</a> </li>
+        <li><a href="H02/zinnen.php">Hoofdstuk 2</a> </li>
+        <li><a href="H02/zinnen2.php">Hoofdstuk 2</a> </li>
+        <li><a href="H02/zinnen3.php">Hoofdstuk 2</a> </li>
+        <li><a href="H02/zinnen4.php">Hoofdstuk 2</a> </li>
+        <li><a href="H03/Opdracht%201.php">Hoofdstuk 3</a> </li>
+        <li><a href="H03/Opdracht2.php">Hoofdstuk 3</a> </li>
+        <li><a href="H03/Opdracht3.php">Hoofdstuk 3</a> </li>
+        <li><a href="H03/Opdracht4.php">Hoofdstuk 3</a> </li>
+    </ul>
+
+</div>
 </body>
 </html>
