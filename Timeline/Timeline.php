65d7210b619eb6eb8dbb7bc2f875a2e93581eec1
Terique915 <54835492+Terique915@users.noreply.github.com>
2021-09-03 12:01:17 +0200

    php

---
+++ Timeline/Timeline.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="Timeline.css">
+    <title>Document</title>
+</head>
+<style>
+    table{
+        border-spacing: 30px;
+    }
+</style>
+<body>
+<table border="1px">
+    <tr id="1" style="background-color: red" >
+        <td>SEPTEMBER</td>
+        <td>OKTOBER</td>
+        <td>NOVEMBER</td>
+        <td>DECEMBER</td>
+
+    </tr>
+    <tr>
+        <td>PHP<br>
+            MY SQL TOETS 2 <br>
+            Database Ontwerpen
+        </td>
+        <td>KEUZE DEEL 1</td>
+        <td>KEUZE DEEL 2<br> STAGE ZOEKEN</td>
+        <td>KEUZE DEEL 3<br> STAGE ZOEKEN<</td>
+
+    </tr>
+</table>
+
+
+</body>
+</html>
