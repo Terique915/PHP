22eb6664c2d8c18bb5d222ea79a05ad97f0f11e5
Terique915 <54835492+Terique915@users.noreply.github.com>
2021-11-08 14:56:44 +0100

    H7 af

--- PHP/H07/website.php
+++ PHP/H07/website.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if (isset($_SESSION)){
-        echo "<h1>Welkom op de website</h1>";
+        echo "<h1>Welkom op de website jij bent een gebruiker</h1>";
         echo"<p><a href='Login.php'>loguit</a></p>";
     }
     else{
