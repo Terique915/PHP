7e7b0ee2e10d64b54a18cd59c5bebb6a11fa4c8b
Terique915 <54835492+Terique915@users.noreply.github.com>
2023-03-29 13:04:18 +0200

    PHP Af just finishing touches

--- PHP/H07/Login.php
+++ PHP/H07/Login.php
@@ -51,7 +51,7 @@ if (isset($_POST['knop'])) {
         }
         elseif ($_POST['pwd'] <>'password'){
             echo "Wrong Password";
-;        }
+       }
 
 
 
--- PHP/H07/website.php
+++ PHP/H07/website.php
@@ -1,9 +1,1 @@
 <?php
-    session_start();
-    if (isset($_SESSION)){
-        echo "<h1>Welkom op de website jij bent een gebruiker</h1>";
-        echo"<p><a href='Login.php'>loguit</a></p>";
-    }
-    else{
-        header('location:Login.php');
-    }
